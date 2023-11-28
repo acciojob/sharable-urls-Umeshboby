1f96378b68e54e86b1cc81dba810e95160bbc556
Umesh Kumar <103483751+Umeshboby@users.noreply.github.com>
2023-11-28 17:35:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // your code here
+let heading = document.getElementById("url");
+let form = document.querySelector("form");
+let nameText = document.getElementById("name");
+let year = document.getElementById("year");
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event){
+    event.preventDefault();
+    
+    if(nameText.value !== "" && year.value !== ""){
+        heading.innerText = heading.innerText + `?name=${nameText.value}&year=${year.value}`;
+        console.log(heading.innerText);
+    }
+    else if (nameText.value == "" && year.value !== ""){
+        heading.innerText = heading.innerText + `?year=${year.value}`;
+        console.log(heading.innerText);
+    }
+    else if(nameText.value !== "" && year.value == ""){
+        heading.innerText = heading.innerText + `?name=${nameText.value}`;
+        console.log(heading.innerText);
+    }
+}
